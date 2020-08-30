1301e47b7e722a6fa2c915148c40242595239d51
Rafael Eduardo Lima <rafael.santos@midway.com.br>
2020-08-30 14:33:25 -0300

    share quando clica e segura no gif listasdo, e ja implementado na pagina de ver

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:buscador_gifs/ui/gif_page.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:share/share.dart';
 import 'package:transparent_image/transparent_image.dart';
 
 class HomePage extends StatefulWidget {
@@ -113,20 +114,25 @@ class _HomePageState extends State<HomePage> {
         itemBuilder: (context, index) {
           if (_search == null || index < snapshot.data["data"].length)
             return GestureDetector(
-                //GestureDetector deixa clicar na imagem
-                child: FadeInImage.memoryNetwork(
-                    placeholder: kTransparentImage,
-                    image: snapshot.data["data"][index]["images"]
-                        ["fixed_height"]["url"],
-                    height: 300.0,
-                    fit: BoxFit.cover),
-                onTap: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) =>
-                              GifPage(snapshot.data["data"][index])));
-                });
+              //GestureDetector deixa clicar na imagem
+              child: FadeInImage.memoryNetwork(
+                  placeholder: kTransparentImage,
+                  image: snapshot.data["data"][index]["images"]["fixed_height"]
+                      ["url"],
+                  height: 300.0,
+                  fit: BoxFit.cover),
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            GifPage(snapshot.data["data"][index])));
+              },
+              onLongPress: () {
+                Share.share(snapshot.data["data"][index]["images"]
+                    ["fixed_height"]["url"]);
+              },
+            );
           else
             return Container(
               child: GestureDetector(
